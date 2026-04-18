ea3b98c672239287ad6b20358701b8756430723c
Tiger <tiger.huang2009@gmail.com>
2026-04-18 14:16:08 -0700

    fixed small bug and added screenshots to README

--- tools/simple_frontend_server.py
+++ tools/simple_frontend_server.py
@@ -87,6 +87,9 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
     def end_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
         super().end_headers()
 
     def log_message(self, format, *args):
